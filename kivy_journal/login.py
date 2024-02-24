efc04d7cd0bb3703581a69b7eba9c86932e32769
rostislav <115248406+cheapdramas@users.noreply.github.com>
2024-02-24 12:17:35 +0200

    Add files via upload

---
+++ kivy_journal/login.py
@@ -0,0 +1,37 @@
+from kivy.lang import Builder
+from kivy.core.window import Window
+import os
+from kivy.network.urlrequest import UrlRequest
+from kivymd.uix.screen import MDScreen
+from user import User
+
+
+user = User()
+class Login(MDScreen):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        
+
+    Builder.load_file(os.getcwd() + '//kvfiles' + "//login.kv")
+    def login_legitcheck(self):
+        self.login_input =self.ids.login_input.text
+        self.password_input = self.ids.password.text
+        request = UrlRequest('http://127.0.0.1:5000/students_log',on_success=self.success_log)
+        
+
+    def success_log(self,daemon,data_for_log):
+        #data = [[1,login,password],[2,login,password]]
+        
+        for list_info in data_for_log:
+            
+            
+            if self.login_input == list_info[1] and self.password_input == list_info[2]:
+
+                self.retranslate_to_schedule_if_success(True)
+                user.id_(list_info[0])
+                print(user.id)
+                
+
+    def retranslate_to_schedule_if_success(self,retranslate):
+        if retranslate:
+            self.manager.current = 'schedule'
---
+++ kivy_journal/user.py
@@ -0,0 +1,10 @@
+
+
+class User():
+    def __init__(self):
+        self.id = 0
+
+
+    def id_(self,id):
+        self.id = id
+       
